16fff91b6faf27f7c9fc1e45a089aa6d16c559bd
RedTint <algefalmocera@gmail.com>
2015-05-23 06:28:03 +0800

    Add resistoflex updates

---
+++ sites/all/themes/custom/resistoflex/js/resistoflex.js
@@ -0,0 +1,16 @@
+jQuery( document ).ready(function( $ ) {	
+  $( '#my-slider' ).sliderPro({
+  		width: "100%",
+			height: 500,
+			fade: true,
+			arrows: true,
+			buttons: true,
+			fullScreen: true,
+			shuffle: true,
+			smallSize: 500,
+			mediumSize: 1000,
+			largeSize: 3000,
+			thumbnailArrows: true,
+			autoplay: true
+  });
+});
--- sites/all/themes/custom/resistoflex/layouts/project_layout/project-layout.tpl.php
+++ sites/all/themes/custom/resistoflex/layouts/project_layout/project-layout.tpl.php
@@ -4,43 +4,43 @@
       <?php print $content['title']; ?>
     <?php endif; ?>
 
+    <?php if (!empty($content['photo']) && $content['photo'] != '&nbsp;'): ?>
+      <div class="project-photos">
+        <?php print $content['photo']; ?>
+      </div>
+    <?php endif; ?>
+
     <?php if (!empty($content['description']) && $content['description'] != '&nbsp;'): ?>
       <div class="project-description">
         <?php print $content['description']; ?>
       </div>
     <?php endif; ?>
 
-    <?php if (!empty($content['client']) && $content['client'] != '&nbsp;'): ?>
+    <?php //if (!empty($content['client']) && $content['client'] != '&nbsp;'): ?>
       <div class="project-client">
-        <?php print $content['client']; ?>
+        <?php //print $content['client']; ?>
       </div>
-    <?php endif; ?>
+    <?php //endif; ?>
 
-    <?php if (!empty($content['location']) && $content['location'] != '&nbsp;'): ?>
+    <?php //if (!empty($content['location']) && $content['location'] != '&nbsp;'): ?>
       <div class="project-location">
-        <?php print $content['location']; ?>
+        <?php //print $content['location']; ?>
       </div>
-    <?php endif; ?>
+    <?php //endif; ?>
   </div>
 
-  <?php if (!empty($content['photo']) && $content['photo'] != '&nbsp;'): ?>
-    <div class="project-photos">
-      <?php print $content['photo']; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if (!empty($content['used_products']) && $content['used_products'] != '&nbsp;'): ?>
+  <?php //if (!empty($content['used_products']) && $content['used_products'] != '&nbsp;'): ?>
     <div class="project-used_products">
-      <h3>Used Products</h3>
-      <?php print $content['used_products']; ?>
+      <!--<h3>Used Products</h3>-->
+      <?php //print $content['used_products']; ?>
     </div>
-  <?php endif; ?>
+  <?php //endif; ?>
 
-   <?php if (!empty($content['availed_services']) && $content['availed_services'] != '&nbsp;'): ?>
+   <?php //if (!empty($content['availed_services']) && $content['availed_services'] != '&nbsp;'): ?>
     <div class="project-availed_services">
-      <h3>Availed Services</h3>
-      <?php print $content['availed_services']; ?>
+      <!--<h3>Availed Services</h3>-->
+      <?php //print $content['availed_services']; ?>
     </div>
-  <?php endif; ?>
+  <?php //endif; ?>
 
 </div>
